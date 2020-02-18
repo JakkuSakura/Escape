1528ac842f59a77dcd91da0b4e1bc8f18f83d22f
qiujiangkun <qiujiangkun@foxmail.com>
2020-02-18 16:28:58 +0800

    init

--- src/client.cpp
+++ src/client.cpp
@@ -1,4 +1,30 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+void initGLFW()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        exit(-1);
+    }
+    glfwMakeContextCurrent(window);
+}
 int main()
 {
+    initGLFW();
+    glewExperimental = GL_TRUE;
+    if (glewInit())
+    {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
     return 0;
 }
