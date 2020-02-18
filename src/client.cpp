1ccf100e7eb54a80d56ccab0d9a47d86b8da3504
qiujiangkun <qiujiangkun@foxmail.com>
2020-02-18 16:56:15 +0800

    window

--- src/client.cpp
+++ src/client.cpp
@@ -1,14 +1,19 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+GLFWwindow *window;
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 void initGLFW()
 {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "Escape", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -16,15 +21,47 @@ void initGLFW()
         exit(-1);
     }
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 }
-int main()
+void initGLEW()
 {
-    initGLFW();
     glewExperimental = GL_TRUE;
     if (glewInit())
     {
         std::cerr << "Failed to initialize GLEW" << std::endl;
-        return -1;
+        exit(-1);
     }
+}
+void initOpenGL()
+{
+    glViewport(0, 0, 800, 600);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+void initAll()
+{
+    initGLFW();
+    initGLEW();
+    initOpenGL();
+}
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+void render()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+int main()
+{
+    initAll();
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+        render();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
     return 0;
 }
