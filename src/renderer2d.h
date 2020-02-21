f8b62d9bccc45829737b85a441ef9b0c832ffa91
qiujiangkun <qiujiangkun@foxmail.com>
2020-02-21 15:03:11 +0800

    run.sh

--- src/renderer2d.h
+++ src/renderer2d.h
@@ -2,53 +2,20 @@
 #define RENDERER2D_H
 #include "shader.h"
 #include "scene.h"
-
+#include "sprite2d.h"
 namespace Escape
 {
 class Renderer2D
 {
 public:
-    Renderer2D() : shader("assets/shaders/point_and_color.vs", "assets/shaders/color.fs")
+    Renderer2D() : shader("assets/shaders/point_and_color.vs", "assets/shaders/color.fs"),
+                   rect(-1, -1, 2, 2, 1.0f, 0, 0)
     {
-        init();
-    }
-    void init()
-    {
-        // set up vertex data (and buffer(s)) and configure vertex attributes
-        // ------------------------------------------------------------------
-        float vertices[] = {
-            0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f,   // top right
-            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom right
-            -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
-            -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  // top left
-        };
-        unsigned int indices[] = {
-            0, 1, 3, // first Triangle
-            1, 2, 3  // second Triangle
-        };
-        glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
-        glGenBuffers(1, &EBO);
-
-        glBindVertexArray(VAO);
-
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
-        glEnableVertexAttribArray(1);
-
     }
 
     void drawRect()
     {
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        rect.draw();
     }
     void applyShader(Shader &shader)
     {
@@ -60,10 +27,12 @@ public:
         last_scene = &scene;
         scene.apply(shader);
     }
+
 private:
-    unsigned int VBO, VAO, EBO;
     Shader shader;
     Scene *last_scene;
+
+    Rectangle rect;
 };
 
 } // namespace Escape
--- src/sprite2d.h
+++ src/sprite2d.h
@@ -6,12 +6,51 @@ namespace Escape
 class Sprite2D
 {
 public:
+    virtual void draw() = 0;
     virtual ~Sprite2D() {}
 };
-class Rectangle : public Sprite2D {
+class Rectangle : public Sprite2D
+{
+    unsigned int VBO, VAO, EBO;
+    float x, y, width, height;
+    float r, g, b;
 
+public:
+    Rectangle(float x, float y, float width, float height, float r, float g, float b) : x(x), y(y), width(width), height(height), r(r), g(g), b(b)
+    {
+        float vertices[] = {
+            x + width, y + height, 0.0f, r, g, b, // top right
+            x + width, y, 0.0f, r, g, b,          // bottom right
+            x, y, 0.0f, r, g, b,                  // bottom left
+            x, y + height, 0.0f, r, g, b          // top left
+        };
+        unsigned int indices[] = {
+            0, 1, 3, // first Triangle
+            1, 2, 3  // second Triangle
+        };
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+    }
+    void draw() override
+    {
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    }
 };
 } // namespace Escape
-
 
 #endif // SPRITE2D_H
