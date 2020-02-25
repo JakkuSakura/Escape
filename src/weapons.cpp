4b46cd162c975379b8d2d2368cbb66cd1cee29ca
qiujiangkun <qiujiangkun@foxmail.com>
2020-02-25 22:47:37 +0800

    shooting speed bug fixed

--- src/weapons.cpp
+++ src/weapons.cpp
@@ -1,110 +1,122 @@
 #include "weapons.h"
 
-namespace Escape {
+namespace Escape
+{
 
+Escape::BulletSystem::BulletSystem()
+{
+}
 
-    Escape::BulletSystem::BulletSystem() {
-    }
+void Escape::BulletSystem::initialize()
+{
+    ECSSystem::initialize();
+    lifespan = findSystem<LifespanSystem>();
+}
 
-    void Escape::BulletSystem::initialize() {
-        ECSSystem::initialize();
-        lifespan = findSystem<LifespanSystem>();
-    }
+void Escape::BulletSystem::fire(Entity firer, BulletType type, float angle, float speed, float damage)
+{
+    Entity bullet = world->create();
+    world->assign<Name>(bullet, "bullet");
+    world->assign<BulletData>(bullet, BulletData{firer_id : world->get<ID>(
+                                                                     firer)
+                                                     .id,
+                                                 type : type,
+                                                 damage : damage,
+                                                 hit : false});
+    world->assign<Velocity>(bullet, speed * cos(angle), speed * sin(angle));
+    world->assign<Position>(bullet, world->get<Position>(firer));
+    world->assign<Lifespan>(bullet, lifespan->period(3));
+}
 
-    void Escape::BulletSystem::fire(Entity firer, BulletType type, float angle, float speed, float damage) {
-        Entity bullet = world->create();
-        world->assign<Name>(bullet, "bullet");
-        world->assign<BulletData>(bullet, BulletData{firer_id : world->get<ID>(
-                firer).id, type : type, damage : damage, hit : false});
-        world->assign<Velocity>(bullet, speed * cos(angle), speed * sin(angle));
-        world->assign<Position>(bullet, world->get<Position>(firer));
-        world->assign<Lifespan>(bullet, lifespan->period(3));
-    }
+void Escape::BulletSystem::update(clock_type delta)
+{
+    world->view<Hitbox, Position, Health, ID>().each(
+        [&](Entity ent, auto &hitbox, auto &position, auto &health, auto id) {
+            if (health.health <= 0)
+                return;
 
-    void Escape::BulletSystem::update(clock_type delta) {
-        world->view<Hitbox, Position, Health, ID>().each(
-                [&](Entity ent, auto &hitbox, auto &position, auto &health, auto id) {
-                    if (health.health <= 0)
+            world->view<BulletData, Position>().each(
+                [&](Entity bullet, auto &bullet_data, auto &position_bullet) {
+                    if (bullet_data.firer_id == id.id)
                         return;
-
-                    world->view<BulletData, Position>().each(
-                            [&](Entity bullet, auto &bullet_data, auto &position_bullet) {
-                                if (bullet_data.firer_id == id.id)
-                                    return;
-                                if (glm::distance2(position_bullet.unwrap(), position.unwrap()) <=
-                                    hitbox.radius * hitbox.radius) // hit
-                                {
-                                    health.health -= bullet_data.damage;
-                                    bullet_data.hit = true;
-                                    world->destroy(bullet);
-                                    return;
-                                }
-                            });
+                    if (glm::distance2(position_bullet.unwrap(), position.unwrap()) <=
+                        hitbox.radius * hitbox.radius) // hit
+                    {
+                        health.health -= bullet_data.damage;
+                        bullet_data.hit = true;
+                        world->destroy(bullet);
+                        return;
+                    }
                 });
-    }
+        });
+}
 
-    Escape::WeaponSystem::WeaponSystem() {
-        default_weapons[WeaponType::HANDGUN] = WeaponPrototype{
-                type : WeaponType::HANDGUN,
-                bullet_type : BulletType::HANDGUN_BULLET,
-                cd : 0.5,
-                accuracy : 95,
-                peice_number : 1,
-                bullet_damage : 10,
-                bullet_speed : 300,
-        };
-        default_weapons[WeaponType::SHOTGUN] = WeaponPrototype{
-                type : WeaponType::SHOTGUN,
-                bullet_type : BulletType::SHOTGUN_SHELL,
-                cd : 1.5,
-                accuracy : 80,
-                peice_number : 10,
-                bullet_damage : 8,
-                bullet_speed : 300,
-        };
-        default_weapons[WeaponType::SMG] = WeaponPrototype{
-                type : WeaponType::SMG,
-                bullet_type : BulletType::SMG_BULLET,
-                cd : 1.0 / 40,
-                accuracy : 85,
-                peice_number : 1,
-                bullet_damage : 3,
-                bullet_speed : 300,
-        };
-        default_weapons[WeaponType::RIFLE] = WeaponPrototype{
-                type : WeaponType::RIFLE,
-                bullet_type : BulletType::RIFLE_BULLET,
-                cd : 2,
-                accuracy : 97,
-                peice_number : 1,
-                bullet_damage : 55,
-                bullet_speed : 500,
-        };
-    }
+Escape::WeaponSystem::WeaponSystem()
+{
+    default_weapons[WeaponType::HANDGUN] = WeaponPrototype{
+        type : WeaponType::HANDGUN,
+        bullet_type : BulletType::HANDGUN_BULLET,
+        cd : 0.5,
+        accuracy : 95,
+        peice_number : 1,
+        bullet_damage : 10,
+        bullet_speed : 300,
+    };
+    default_weapons[WeaponType::SHOTGUN] = WeaponPrototype{
+        type : WeaponType::SHOTGUN,
+        bullet_type : BulletType::SHOTGUN_SHELL,
+        cd : 1.5,
+        accuracy : 80,
+        peice_number : 10,
+        bullet_damage : 8,
+        bullet_speed : 300,
+    };
+    default_weapons[WeaponType::SMG] = WeaponPrototype{
+        type : WeaponType::SMG,
+        bullet_type : BulletType::SMG_BULLET,
+        cd : 1.0 / 30,
+        accuracy : 85,
+        peice_number : 1,
+        bullet_damage : 4,
+        bullet_speed : 300,
+    };
+    default_weapons[WeaponType::RIFLE] = WeaponPrototype{
+        type : WeaponType::RIFLE,
+        bullet_type : BulletType::RIFLE_BULLET,
+        cd : 2,
+        accuracy : 97,
+        peice_number : 1,
+        bullet_damage : 55,
+        bullet_speed : 500,
+    };
+}
 
-    void Escape::WeaponSystem::initialize() {
-        ECSSystem::initialize();
-        bullet_system = findSystem<BulletSystem>();
-        timeserver = findSystem<TimeServer>();
-    }
+void Escape::WeaponSystem::initialize()
+{
+    ECSSystem::initialize();
+    bullet_system = findSystem<BulletSystem>();
+    timeserver = findSystem<TimeServer>();
+}
 
-    void Escape::WeaponSystem::fire(Entity ent, float angle) {
-        if (world->has<Weapon>(ent)) {
-            auto weapon = world->get<Weapon>(ent);
-
+void Escape::WeaponSystem::fire(Entity ent, float angle)
+{
+    if (world->has<Weapon>(ent))
+    {
+        auto &weapon = world->get<Weapon>(ent);
+        if (timeserver->now() >= weapon.next)
+        {
             const WeaponPrototype &prototype = default_weapons.at(weapon.weapon);
-            if (timeserver->now() >= weapon.next) {
-                float angle_diff = std::max(0.0, M_PI_4 * (100 - prototype.accuracy) / 100);
+            float angle_diff = std::max(0.0, M_PI_4 * (100 - prototype.accuracy) / 100);
 
-                for (size_t i = 0; i < prototype.peice_number; i++) {
+            for (size_t i = 0; i < prototype.peice_number; i++)
+            {
 
-                    bullet_system->fire(ent, prototype.bullet_type, angle + timeserver->random(-angle_diff, angle_diff),
-                                        prototype.bullet_speed, prototype.bullet_damage);
-                }
-                weapon.last = timeserver->now();
-
-                weapon.next = timeserver->now() + prototype.cd;
+                bullet_system->fire(ent, prototype.bullet_type, angle + timeserver->random(-angle_diff, angle_diff),
+                                    prototype.bullet_speed, prototype.bullet_damage);
             }
+            weapon.last = timeserver->now();
+            weapon.next = timeserver->now() + prototype.cd;
         }
     }
 }
+} // namespace Escape
