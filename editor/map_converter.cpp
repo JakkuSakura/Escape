bfafa15f457110717f4d2f64858fba4550c54384
qiujiangkun <qiujiangkun@foxmail.com>
2020-03-01 03:37:44 +0800

    map converter

---
+++ editor/map_converter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jack on 20-3-1.
+//
+
+#include "map_converter.h"
---
+++ editor/map_converter.h
@@ -0,0 +1,16 @@
+//
+// Created by jack on 20-3-1.
+//
+
+#ifndef ESCAPE_MAP_CONVERTER_H
+#define ESCAPE_MAP_CONVERTER_H
+
+
+
+class map_converter {
+
+};
+
+
+
+#endif //ESCAPE_MAP_CONVERTER_H
